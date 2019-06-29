891b31217204bb7d60a99a116b156971c0a2d558
Silas S. Brown <ssb22@cam.ac.uk>
2019-06-29 07:38:09 +0100

    update

--- git-sync.sh
+++ git-sync.sh
@@ -4,7 +4,7 @@ wget -N http://people.ds.cam.ac.uk/ssb22/mwrhome/jianpu-ly.py
 
 (
     awk -- 'BEGIN {p=1} /^Run jianpu-ly / {p=0} // {if(p) print}' < README.md
-    python jianpu-ly.py --markdown
+    python jianpu-ly.py --markdown | awk -- 'BEGIN {p=0} /^Run jianpu-ly / {p=1} // {if(p) print}'
     ) > n && mv n README.md
 
 git commit -am update && git push
