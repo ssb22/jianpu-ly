55bd3f86b76a95f9cdcb0c20f03be0990f495060
Silas S. Brown <ssb22@cam.ac.uk>
2020-04-07 08:27:19 +0100

    Update jianpu-ly.py

--- jianpu-ly.py
+++ jianpu-ly.py
@@ -2,7 +2,7 @@
 # (can be run with either Python 2 or Python 3)
 
 # Jianpu (numbered musical notaion) for Lilypond
-# v1.401 (c) 2012-2020 Silas S. Brown
+# v1.402 (c) 2012-2020 Silas S. Brown
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -654,7 +654,7 @@ def getLY(score):
                 if midi or western: out.append(r"\grace { " + gracenotes_western(word[2:-1]) + " }")
                 else:
                     aftrnext = graceNotes_markup(word[2:-1],0)
-                    if not self.withStaff: out.append(r"\once \textLengthOn ")
+                    if not notehead_markup.withStaff: out.append(r"\once \textLengthOn ")
                     if not defined_jianpuGrace:
                         defined_jianpuGrace = True
                         out.append(r"""#(define-markup-command (jianpu-grace layout props text)
@@ -684,7 +684,7 @@ def getLY(score):
             elif word.startswith("[") and word.endswith("]g"):
                 if midi or western: out[lastPtr] = r" \afterGrace { " + out[lastPtr] + " } { " + gracenotes_western(word[1:-2]) + " }"
                 else:
-                    if not self.withStaff:
+                    if not notehead_markup.withStaff:
                         out[lastPtr] = r"\once \textLengthOn " + out[lastPtr]
                     out.insert(lastPtr+1,graceNotes_markup(word[1:-2],1))
                     if not defined_JGR:
